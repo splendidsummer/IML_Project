237d1cc8851b0a53e94b03c8c4a035e8dfd8252a
splendidsummer <417846472@qq.com>
2021-11-20 23:42:49 +0100

    Closing debug for fuzzy c means code

--- models/fuzzy_c_means.py
+++ models/fuzzy_c_means.py
@@ -26,18 +26,18 @@ class fuzzyCMeans:
     def initialize_membership_matrix(self):
 
         self.membership_matrix = self.random_generator.uniform(size=(self.n_samples, self.k_clusters))
-        membership_normalizer = self.membership_matrix.sum(axis=1)[np.newaxis].T
-        membership_normalizer = np.tile(membership_normalizer, self.k_clusters)
-
+        membership_normalizer = self.membership_matrix.sum(axis=1, keepdims=True)
         self.membership_matrix = self.membership_matrix / membership_normalizer
+        # self.membership_matrix = self.membership_matrix.T
 
     def find_centroids(self):
         """Update cluster centers"""
         u_power_m = self.membership_matrix ** self.m
         numerator = np.dot(self.data.T, u_power_m)
-        denominator = np.sum(u_power_m, axis=0)  # sum the u_ij values of all samples in cluster i
+        denominator = u_power_m.sum(axis=0)
 
-        self.centroids = (numerator/denominator).T
+        self.centroids = (numerator/denominator)
+        self.centroids = self.centroids.T
 
     def update_membership_matrix(self):
         """ Compute the new membership or U matrix
@@ -54,11 +54,9 @@ class fuzzyCMeans:
         dist_samples_centroid = self._compute_distance()
         dist_samples_centroid = dist_samples_centroid ** float(2/(self.m-1))
         reshape_dist = dist_samples_centroid.reshape((self.data.shape[0], 1, -1))  # add a new dimension in the middle
-        # expand the second dimension from 1 to
         dist_on_all_centroids = reshape_dist.repeat(reshape_dist.shape[-1], axis=1)
         denominator = dist_samples_centroid[:, :, np.newaxis] / dist_on_all_centroids
-
-        return 1 / denominator.sum(2)
+        self.membership_matrix = 1 / denominator.sum(2)
 
     def _compute_distance(self):
         """  Using "_compute_distance" to calculate the distance between sample and centroid
@@ -90,11 +88,9 @@ class fuzzyCMeans:
         return distances
 
     def get_inference(self, inputs):
-        inputs = np.expand_dims(inputs, axis=0) if len(inputs.shape) == 1 else inputs
-        dist = self._compute_inference_distance(inputs)
-        temp = dist ** float(2 / (self.m - 1))
-        denominator_ = temp.reshape((inputs.shape[0], 1, -1)).repeat(temp.shape[-1], axis=1)
-        denominator_ = temp[:, :, np.newaxis] / denominator_
-        fuzzy_results = 1 / denominator_.sum(2)
-        de_fuzzy_results = fuzzy_results.argmax(axis=-1)
-        return de_fuzzy_results
+        results = np.full((inputs.shape[0], 1), -1)
+        for i in range(inputs.shape[0]):
+            sample_distances = np.sqrt(np.sum(np.power(self.centroids - inputs[i][np.newaxis, :], 2), axis=1))
+            curr_label = np.argmin(sample_distances)
+            results[i] = curr_label
+        return results
--- test_train.py
+++ test_train.py
@@ -8,17 +8,18 @@ inputs3 = np.random.randn(100, 10) + 5.0
 
 test_data1 = np.random.randn(10, 10)
 test_data2 = np.random.randn(10, 10) + 5.
+test_data3 = np.random.randn(10, 10) + 10.
 test_data = np.vstack((test_data1, test_data2))
+test_data = np.vstack((test_data, test_data3))
 
 
 inputs = np.vstack((inputs1, inputs2))
 inputs = np.vstack((inputs, inputs3))
 
-cluster = fuzzyCMeans(3, inputs, 100, random_state)
-#
-# cluster.init_centroids()
-# cluster.train()
-# inferences = cluster.get_inference(test_data)
+cluster = fuzzyCMeans(3, inputs, 33, max_iter=100, epsilon=1e-5)
+
+cluster.train()
+inferences = cluster.get_inference(test_data)
 
 
 
